ddc1cb8ff8674ea7007fa87a28fe203ec668c5e5
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-10-17 17:27:57 -0400

    Updates

---
+++ precache-manifest.6ad9ccb5217f9b5c3a338077ff766d9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "365113492f7e2ffdf10d",
+    "url": "/react-redux-quote-machine/static/js/runtime~main.36511349.js"
+  },
+  {
+    "revision": "ab2b2818fcacf23db733",
+    "url": "/react-redux-quote-machine/static/js/main.ab2b2818.chunk.js"
+  },
+  {
+    "revision": "61c1d353a5934b824063",
+    "url": "/react-redux-quote-machine/static/js/1.61c1d353.chunk.js"
+  },
+  {
+    "revision": "ab2b2818fcacf23db733",
+    "url": "/react-redux-quote-machine/static/css/main.c28f8235.chunk.css"
+  },
+  {
+    "revision": "b807572446f0f2a4d4bfac0d6d42e75f",
+    "url": "/react-redux-quote-machine/index.html"
+  }
+];
